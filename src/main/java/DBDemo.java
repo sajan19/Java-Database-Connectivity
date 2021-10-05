949eda44c2944a7b86724f51fb2c6d2372c40734
Sajan <sajanmhatre29@gmail.com>
2021-10-06 00:02:37 +0530

    Database Connection

--- src/main/java/DBDemo.java
+++ src/main/java/DBDemo.java
@@ -1,5 +1,40 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 public class DBDemo {
+    //Main Method
     public static void main(String[] args) {
         System.out.println("Welcome to Java DataBase Connectivity...");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Sajan@2265";
+        Connection con;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath", e);
+        }
+        //Calling of Method
+        listDrivers();
+        try {
+            System.out.println("Connecting to database:" + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is Successful!!! " + con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //Method to get List of Drivers
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("  " + driverClass.getClass().getName());
+        }
     }
 }
+
